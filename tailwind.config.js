2805c0083b9c82265c23e6f79883572ffd50c774
Adrian Frąckowiak <67045837+adrianfrackowiak@users.noreply.github.com>
2022-01-19 09:13:07 +0100

    add extended colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        getbold: {
+          blue: "#233176",
+          pink: "#FF7288",
+          bg: "#FFFFFF",
+          "btn-pink": "#EF6278",
+          "btn-blue": "#3B5AA0",
+        },
+      },
+    },
   },
   plugins: [],
 };
