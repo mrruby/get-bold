65f90f1abb5aaa86fb1413f624e90f7866d7c080
Adrian Frąckowiak <67045837+adrianfrackowiak@users.noreply.github.com>
2022-01-19 09:03:43 +0100

    set up typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,36 @@
 module.exports = {
   siteMetadata: {
-      title: `GetBold`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `GetBold`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+  ],
 };
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>GetBold</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
