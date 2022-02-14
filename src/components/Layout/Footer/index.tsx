2b426a67f6687a0acf51be43bff192161774728c
Adrian Frąckowiak <67045837+adrianfrackowiak@users.noreply.github.com>
2022-02-14 16:32:41 +0100

    add lorem ipsum to empty pages

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -119,8 +119,14 @@ const Footer: React.FC<Props> = ({ isWhiteBg }) => {
       <div className="w-full h-[5.125rem] mt-[87px] bg-[#FFAAB7] absolute left-0">
         <nav className="2xl:px-[21rem] px-8 xl:px-[150px] h-full">
           <ul className="flex items-center space-x-12 text-getbold-btn-pink h-full">
-            <li>Polityka Prywatności</li>
-            <li>Polityka ochrony danych osobowych</li>
+            <li>
+              <Link to="/polityka-prywatnosci">Polityka Prywatności</Link>
+            </li>
+            <li>
+              <Link to="/polityka-ochrony-danych-osobowych">
+                Polityka ochrony danych osobowych
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
