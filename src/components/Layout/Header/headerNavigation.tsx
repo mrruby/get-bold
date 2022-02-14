9dc1d80e678b537cf7c1f5fe5e145b1aa8b2576e
Adrian Frąckowiak <67045837+adrianfrackowiak@users.noreply.github.com>
2022-02-14 14:11:14 +0100

    add hovers to menu

--- src/components/Layout/Header/headerNavigation.tsx
+++ src/components/Layout/Header/headerNavigation.tsx
@@ -5,22 +5,70 @@ export const HeaderNavigation = () => {
   return (
     <nav className="hidden lg:block">
       <ul className="flex space-x-2 lg:space-x-12 font-bold items-center">
-        <Link to="/">
+        <Link
+          to="/"
+          activeStyle={{
+            color: "#EF6278",
+            fontWeight: 700,
+            borderBottom: "2px solid #EF6278",
+          }}
+          className="py-1 hover:text-[#EF6278] hover:border-b-2 hover:border-[#EF6278]"
+        >
           <li>Home</li>
         </Link>
-        <Link to="/oferta">
+        <Link
+          to="/oferta"
+          activeStyle={{
+            color: "#EF6278",
+            fontWeight: 700,
+            borderBottom: "2px solid #EF6278",
+          }}
+          className="py-1 hover:text-[#EF6278] hover:border-b-2 hover:border-[#EF6278]"
+        >
           <li>Oferta</li>
         </Link>
-        <Link to="/o-nas">
+        <Link
+          to="/o-nas"
+          activeStyle={{
+            color: "#EF6278",
+            fontWeight: 700,
+            borderBottom: "2px solid #EF6278",
+          }}
+          className="py-1 hover:text-[#EF6278] hover:border-b-2 hover:border-[#EF6278]"
+        >
           <li>O Nas</li>
         </Link>
-        <Link to="/portfolio">
+        <Link
+          to="/portfolio"
+          activeStyle={{
+            color: "#EF6278",
+            fontWeight: 700,
+            borderBottom: "2px solid #EF6278",
+          }}
+          className="py-1 hover:text-[#EF6278] hover:border-b-2 hover:border-[#EF6278]"
+        >
           <li>Portfolio</li>
         </Link>
-        <Link to="/blog">
+        <Link
+          to="/blog"
+          activeStyle={{
+            color: "#EF6278",
+            fontWeight: 700,
+            borderBottom: "2px solid #EF6278",
+          }}
+          className="py-1 hover:text-[#EF6278] hover:border-b-2 hover:border-[#EF6278]"
+        >
           <li>Blog</li>
         </Link>
-        <Link to="/kontakt">
+        <Link
+          to="/kontakt"
+          activeStyle={{
+            color: "#EF6278",
+            fontWeight: 700,
+            borderBottom: "2px solid #EF6278",
+          }}
+          className="py-1 hover:text-[#EF6278] hover:border-b-2 hover:border-[#EF6278]"
+        >
           <li>Kontakt</li>
         </Link>
       </ul>
